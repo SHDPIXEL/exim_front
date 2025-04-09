b1a218abf3dc3618c88d8bb003d852a2926b8788
SHDPIXEL <shdpixel@gmail.com>
2025-04-09 16:35:10 +0530

    Update FacebookPost.jsx

--- src/pages/HomeBannerSlider/FacebookPost.jsx
+++ src/pages/HomeBannerSlider/FacebookPost.jsx
@@ -31,7 +31,7 @@ const FacebookPost = () => {
         // </div>
 
         <>
-            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshdpixel%2Fposts%2Fpfbid02TKRbnNSUyMTJiFVxwVzMMPrXs2mUocvuBA1dcaKGjJxtHsYQGQqWDCvXEXcn9Sjgl&show_text=true&width=500"
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0npBrFFWEaigSupTSPqFQwj19j7U1MYpVix57w42PCVNnEeeDApXMUmc1ujvRVS6Ll%26id%3D100064039290498&show_text=true&width=500"
              width="100%" 
              height="698" 
              style={{ border:'none', overflow:'hidden' }}
